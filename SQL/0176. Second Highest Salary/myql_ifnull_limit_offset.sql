b93f82c9d6767504de21ba7bc3a31c022a324bf3
NoraCarey <46361714+NoraCarey@users.noreply.github.com>
2021-02-09 13:04:17 -0500

    Create myql_ifnull_limit_offset.sql

---
+++ SQL/0176. Second Highest Salary/myql_ifnull_limit_offset.sql
@@ -0,0 +1,8 @@
+
+
+SELECT IFNULL((SELECT DISTINCT Salary
+                 FROM Employee
+                 ORDER BY Salary DESC
+                 LIMIT 1, 1), NULL) AS SecondHighestSalary;
+                 
+                 
