44f20e022a1e3377a9c285c59c87c2425b18cfea
NoraCarey <46361714+NoraCarey@users.noreply.github.com>
2021-02-17 11:38:25 -0500

    Create mysql_with_rank.sql

---
+++ SQL/0184. Department Highest Salary/mysql_with_rank.sql
@@ -0,0 +1,11 @@
+
+
+WITH cte AS (SELECT d.Name AS Department, e.Name AS Employee, e.Salary AS Salary, 
+                    RANK() OVER(PARTITION BY e.DepartmentId
+                                ORDER BY e.Salary DESC) AS ranking
+             FROM Employee e JOIN Department d ON e.DepartmentId = d.Id)
+
+SELECT Department, Employee, Salary
+FROM cte
+WHERE ranking = 1;
+
