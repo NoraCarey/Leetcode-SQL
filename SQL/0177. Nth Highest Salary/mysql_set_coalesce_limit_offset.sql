4b507b01e9217abfcda94b792b2851cb52726a34
NoraCarey <46361714+NoraCarey@users.noreply.github.com>
2021-02-22 16:33:33 -0500

    Update mysql_set_coalesce_limit_offset.sql

--- SQL/0177. Nth Highest Salary/mysql_set_coalesce_limit_offset.sql
+++ SQL/0177. Nth Highest Salary/mysql_set_coalesce_limit_offset.sql
@@ -1,10 +1,12 @@
 
-# Becasue the N is a local varialbe assigment it is not user defined variable
+# Becasue N is a Local Variable Assignment, it is not a User-defined Variable Assignment, there is no need using @ here
+# It is just SET N := N - 1, ('=' AND ':=' is the same)
+
 
 CREATE FUNCTION getNthHighestSalary(N INT) 
 RETURNS INT
 BEGIN
-SET N = N - 1;
+SET N := N - 1;
   RETURN (
       # Write your MySQL query statement below.
       SELECT COALESCE((SELECT DISTINCT Salary
