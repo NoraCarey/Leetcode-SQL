fe17935ff54250fcc85b2c80d6244d08a8e75bbd
NoraCarey <46361714+NoraCarey@users.noreply.github.com>
2021-02-27 14:20:08 -0500

    Update mysql_limit.sql

--- SQL/0574. Winning Candidate/mysql_limit.sql
+++ SQL/0574. Winning Candidate/mysql_limit.sql
@@ -1,11 +1,13 @@
 
 
+# Assume there will be only one winning candidate
+
 SELECT Name
 FROM Candidate c
-JOIN Vote v ON c.id = v.CandidateId
-GROUP BY Name
+RIGHT JOIN Vote v ON c.id = v.CandidateId
+GROUP BY c.id, c.Name
 ORDER BY COUNT(c.id) DESC
 LIMIT 1;
-      
+
       
       
