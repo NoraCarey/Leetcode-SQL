e8b81706f7b46d3ab0a7318eff5d51dc1f4d8dee
NoraCarey <46361714+NoraCarey@users.noreply.github.com>
2021-02-27 14:20:33 -0500

    Update mysql_rank_window_function.sql

--- SQL/0574. Winning Candidate/mysql_rank_window_function.sql
+++ SQL/0574. Winning Candidate/mysql_rank_window_function.sql
@@ -1,11 +1,13 @@
 
 
-SELECT Name
-FROM (SELECT Name, Rank() OVER(ORDER BY a.vote_nums DESC) AS ranking
-      FROM (SELECT Name, COUNT(c.id) AS vote_nums
-            FROM Candidate c
-            JOIN Vote v ON c.id = v.CandidateId
-            GROUP BY Name) a) b
+# Assume there will be more than one winning candidate
+
+SELECT b.Name
+FROM (SELECT a.id, a.Name, RANK() OVER(ORDER BY a.count_nums DESC) AS ranking
+      FROM (SELECT c.id, c.Name, COUNT(c.id) AS count_nums
+            FROM Candidate c 
+            RIGHT JOIN Vote v ON c.id = v.CandidateId
+            GROUP BY c.id, c.Name) a) b
 WHERE b.ranking = 1;
 
 
