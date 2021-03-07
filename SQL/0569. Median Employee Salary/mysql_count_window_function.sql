53fdc49545d9e963c88ee68adbbed9027ad4987f
NoraCarey <46361714+NoraCarey@users.noreply.github.com>
2021-03-07 17:39:53 -0500

    Update mysql_count_window_function.sql

--- SQL/0569. Median Employee Salary/mysql_count_window_function.sql
+++ SQL/0569. Median Employee Salary/mysql_count_window_function.sql
@@ -12,7 +12,7 @@ FROM (SELECT Id, Company, Salary,
       FROM Employee) tbl
 # select the employees have the median salary in each company 
 # if company have the odd number employees, floor() and ceil() will return to the same number
-# if company have the even number employees, ceil() and ceil() will return to two consecutive numbers
+# if company have the even number employees, floor() and ceil() will return to two consecutive numbers
 WHERE ranking = FLOOR((count_num + 1) / 2) OR 
       ranking = CEIL((count_num + 1) / 2);
 
