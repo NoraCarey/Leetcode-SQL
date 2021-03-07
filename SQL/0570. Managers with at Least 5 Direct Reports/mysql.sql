9b59c6d9b7cb1194fa4f44ce84be227f3bfe9ff4
NoraCarey <46361714+NoraCarey@users.noreply.github.com>
2021-03-07 13:20:34 -0500

    Update mysql.sql

--- SQL/0570. Managers with at Least 5 Direct Reports/mysql.sql
+++ SQL/0570. Managers with at Least 5 Direct Reports/mysql.sql
@@ -1,9 +1,12 @@
 
-SELECT NAME 
-FROM (SELECT ManagerId, COUNT(Id)
-      FROM Employee 
+SELECT Name
+FROM (SELECT ManagerId, COUNT(ManagerId) AS manager_num
+      FROM Employee
       GROUP BY ManagerId
-      HAVING COUNT(Id) >= 5) tbl
+      HAVING manager_num >= 5) tbl
 JOIN Employee e ON tbl.ManagerId = e.Id;
+
+
+
 
 
