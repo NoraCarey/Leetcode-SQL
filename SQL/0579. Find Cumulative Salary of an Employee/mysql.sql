618252788609bc6bb160eb3a78c656415931adeb
NoraCarey <46361714+NoraCarey@users.noreply.github.com>
2021-03-08 16:15:08 -0500

    Update mysql.sql

--- SQL/0579. Find Cumulative Salary of an Employee/mysql.sql
+++ SQL/0579. Find Cumulative Salary of an Employee/mysql.sql
@@ -8,15 +8,14 @@ FROM (SELECT Id,
              # for row1, itself. 
              # for row2, itself plus row1
              # for row3, itself plus row1 and row2
-             # for row4, itself plus row2 and row3
-      
+             # for row4, itself plus row2 and row3   
              Month, SUM(Salary) OVER(PARTITION BY Id
                                      ORDER BY Month
                                      ROWS 2 PRECEDING) AS cumulative_salary,
+      
              # find the the most recent month
              # because we use the LAST_VALUE Window Function, the frame_clause should use RANGE, which is corresponding the row values
              # and because we want to get the last value, we should consider all partition rows
-      
              LAST_VALUE(Month) OVER(PARTITION BY Id
                                     ORDER BY Month
                                     RANGE BETWEEN UNBOUNDED PRECEDING AND 
