29d2dad9f9a136720e576c2651e69bfe49b7cd48
NoraCarey <46361714+NoraCarey@users.noreply.github.com>
2021-03-08 16:05:05 -0500

    Update mysql.sql

--- SQL/0579. Find Cumulative Salary of an Employee/mysql.sql
+++ SQL/0579. Find Cumulative Salary of an Employee/mysql.sql
@@ -1,17 +1,19 @@
 
 
 
-SELECT Id, Month, Salary
-FROM (SELECT Id, Month, SUM(Salary) OVER(PARTITION BY Id
-                                         ORDER BY Month
-                                         ROWS 2 PRECEDING) AS Salary,
+SELECT Id, Month, cumulative_salary as Salary
+FROM (SELECT Id, 
+             # calculate the cumulative salary over a period of 3 months
+             # frame
+             Month, SUM(Salary) OVER(PARTITION BY Id
+                                     ORDER BY Month
+                                     ROWS 2 PRECEDING) AS cumulative_salary,
              LAST_VALUE(Month) OVER(PARTITION BY Id
                                     ORDER BY Month
-                                    ROWS BETWEEN UNBOUNDED PRECEDING 
-                                         AND UNBOUNDED FOLLOWING) AS recent_month
-       FROM Employee) a
+                                    RANGE BETWEEN UNBOUNDED PRECEDING AND 
+                                                  UNBOUNDED FOLLOWING) AS recent_month
+      FROM Employee) tbl
 WHERE Month != recent_month
 ORDER BY Id, Month DESC;
-
 
 
