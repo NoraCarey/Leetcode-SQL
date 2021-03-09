dfc169bed90c1d9d10509597db01c625852d41ff
NoraCarey <46361714+NoraCarey@users.noreply.github.com>
2021-03-09 10:51:43 -0500

    Create mysql_group_by_limit.sql

---
+++ SQL/0586. Customer Placing the Largest Number of Orders/mysql_group_by_limit.sql
@@ -0,0 +1,14 @@
+
+
+
+# # assume there will be only one customer placed the largest number of orders
+
+# SELECT customer_number
+# FROM Orders
+# GROUP BY customer_number
+# ORDER BY COUNT(order_number) DESC
+# LIMIT 1;
+
+
+   
+   
