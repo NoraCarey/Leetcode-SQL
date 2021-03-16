11c8f261ac32b54fa80904b4236f4f7f4bfcfe4e
NoraCarey <46361714+NoraCarey@users.noreply.github.com>
2021-03-16 11:03:48 -0400

    Create mysql_pivot_window_function.sql

---
+++ SQL/0618. Students Report By Geography/mysql_pivot_window_function.sql
@@ -0,0 +1,16 @@
+
+
+   # Using CASE to PIVOT rows to Columns
+   # Using MAX or MIN function within each group 'row_id' to print names where they exist, if not, NULL is printed
+   # MAX function can consider only non-null values
+   
+SELECT MAX(CASE WHEN continent = 'America' THEN name END) AS America,
+       MAX(CASE WHEN continent = 'Asia' THEN name END) AS Asia,
+       MAX(CASE WHEN continent = 'Europe' THEN name END) AS Europe 
+  
+     # Attach a row_id to each Name within its continent
+FROM (SELECT name, continent, ROW_NUMBER() OVER(PARTITION BY continent
+                                                ORDER BY name) AS row_id
+      FROM student) tbl
+GROUP BY row_id;
+
