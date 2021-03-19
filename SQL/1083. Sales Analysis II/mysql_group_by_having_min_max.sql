4f7fdda911255b21ca6f3d33e90d424174f102e6
NoraCarey <46361714+NoraCarey@users.noreply.github.com>
2021-03-19 12:59:30 -0400

    Create mysql_group_by_having_min_max.sql

---
+++ SQL/1083. Sales Analysis II/mysql_group_by_having_min_max.sql
@@ -0,0 +1,8 @@
+
+
+SELECT DISTINCT p.product_id, product_name
+FROM Product p
+JOIN Sales s ON p.product_id = s.product_id
+GROUP BY p.product_id, product_name
+HAVING MIN(sale_date) >= '2019-01-01' AND MAX(sale_date) <= '2019-03-31';
+
