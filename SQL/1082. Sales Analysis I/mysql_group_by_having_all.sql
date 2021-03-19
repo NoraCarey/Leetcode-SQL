e35bb8073451f1aab564676a5f982ccd1e728f35
NoraCarey <46361714+NoraCarey@users.noreply.github.com>
2021-03-19 11:01:15 -0400

    Create mysql_group_by_having_all.sql

---
+++ SQL/1082. Sales Analysis I/mysql_group_by_having_all.sql
@@ -0,0 +1,12 @@
+
+
+
+SELECT seller_id
+FROM Sales
+GROUP BY seller_id
+HAVING SUM(price) >= ALL(SELECT SUM(price)
+                         FROM Sales
+                         GROUP BY seller_id);
+                         
+                         
+                         
