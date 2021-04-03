49d4fb68988a9dd3fb23e2658f0dc27f81331878
NoraCarey <46361714+NoraCarey@users.noreply.github.com>
2021-04-03 18:45:00 -0400

    Create mysql_sum_window_function.sql

---
+++ SQL/1308. Running Total for Different Genders/mysql_sum_window_function.sql
@@ -0,0 +1,9 @@
+
+
+SELECT gender, day, SUM(score_points) OVER(PARTITION BY gender
+                                           ORDER BY day) AS total
+FROM Scores
+GROUP BY gender, day
+ORDER BY gender, day;
+
+
