fe2c55ffe324aa06e8997d79dac86a1db03403da
NoraCarey <46361714+NoraCarey@users.noreply.github.com>
2021-04-04 14:56:16 -0400

    Update mysql_cte_add_new_ranking_table.sql

--- SQL/1336. Number of Transactions per Visit/mysql_cte_add_new_ranking_table.sql
+++ SQL/1336. Number of Transactions per Visit/mysql_cte_add_new_ranking_table.sql
@@ -1,21 +1,28 @@
 
 
 WITH cte AS (SELECT v.user_id, v.visit_date, 
+                    # to  calculate how many transactions happend when the user visit the bank
+                    # using IFNULL() function to return 0 while there is no transaction for the user in the visiting date
                     IFNULL(COUNT(transaction_date), 0) AS transactions_count
-             FROM Transactions t
-             RIGHT JOIN Visits v
-             ON t.user_id = v.user_id AND t.transaction_date = v.visit_date
+             FROM Visits v
+             LEFT JOIN Transactions t
+             ON v.user_id = t.user_id AND v.visit_date = t.transaction_date
              GROUP BY 1, 2),
-     row_nums AS (SELECT ROW_NUMBER() OVER () as ranking 
-                  FROM Transactions 
-                  UNION 
-                  SELECT 0) 
+    # for the result table the transactions_count start from 0 to max(trasactions_count),
+    # we build a new table to record the transactions_count in case the original table missing some numbers
+    row_nums AS (SELECT ROW_NUMBER() OVER() AS num
+                 FROM Transactions
+                 UNION 
+                 SELECT 0) 
 
-SELECT ranking as transactions_count, IFNULL(COUNT(transactions_count), 0) AS visits_count
-FROM cte 
-RIGHT JOIN row_nums ON transactions_count = ranking
-WHERE ranking <= (SELECT MAX(transactions_count) FROM cte)
-GROUP BY ranking
-ORDER BY 1
+SELECT num AS transactions_count, 
+       # using COUNT(transactions_count) to calculate how many visitors did the transactions_count 
+       # IFNULL() function returns 0 while there is no visitors did the transactions_count
+       IFNULL(COUNT(transactions_count), 0) AS visits_count
+FROM row_nums 
+LEFT JOIN cte ON num = transactions_count
+WHERE num <= (SELECT MAX(transactions_count) FROM cte)
+GROUP BY num
+ORDER BY num;
 
 
