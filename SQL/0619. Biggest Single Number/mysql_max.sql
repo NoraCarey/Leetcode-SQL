01d3016f613d4ec194e23848498850ecec6e5a37
NoraCarey <46361714+NoraCarey@users.noreply.github.com>
2021-04-04 22:14:47 -0400

    Update mysql_max.sql

--- SQL/0619. Biggest Single Number/mysql_max.sql
+++ SQL/0619. Biggest Single Number/mysql_max.sql
@@ -1,10 +1,11 @@
 
 
+# using MAX() Function, when there is num, it returns num, else it returns NULL
 SELECT MAX(num) AS num
 FROM (SELECT num
       FROM my_numbers
       GROUP BY num
-      HAVING COUNT(*) = 1) tbl
-      
-      
+      HAVING COUNT(*) = 1) a
+
+
       
