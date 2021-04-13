1fc5074df3df85aaa5ff3148a05e26b1ca3851cc
NoraCarey <46361714+NoraCarey@users.noreply.github.com>
2021-04-13 12:09:28 -0400

    Create mysql_not_in_subquery.sql

---
+++ SQL/1607. Sellers With No Sales/mysql_not_in_subquery.sql
@@ -0,0 +1,11 @@
+
+
+SELECT seller_name
+FROM Seller
+WHERE seller_id NOT IN (SELECT seller_id 
+                        FROM Orders 
+                        WHERE YEAR(sale_date) = 2020
+                        GROUP BY seller_id)
+ORDER BY seller_name ASC;
+
+
