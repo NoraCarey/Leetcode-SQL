7325527da228fcd96621348032a3a5f683479183
NoraCarey <46361714+NoraCarey@users.noreply.github.com>
2021-04-23 14:18:41 -0400

    Create mysql_ifnull_left_join.sql

---
+++ SQL/1677. Product's Worth Over Invoices/mysql_ifnull_left_join.sql
@@ -0,0 +1,10 @@
+
+
+SELECT name, IFNULL(SUM(rest), 0) AS rest, IFNULL(SUM(paid), 0) AS paid,        
+       IFNULL(SUM(canceled), 0) AS canceled, IFNULL(SUM(refunded),0) AS refunded
+FROM Product p
+LEFT JOIN Invoice i ON p.product_id = i.product_id
+GROUP BY name
+ORDER BY name;
+
+
