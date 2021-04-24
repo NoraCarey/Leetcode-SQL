81d66ee247ed498dcadab3b3525b965e35f1bed2
NoraCarey <46361714+NoraCarey@users.noreply.github.com>
2021-04-24 18:12:26 -0400

    Update mysql_without_max_min.sql

--- SQL/1355. Activity Participants/mysql_without_max_min.sql
+++ SQL/1355. Activity Participants/mysql_without_max_min.sql
@@ -1,5 +1,6 @@
 
 
+# Method 1
 SELECT activity
 FROM Friends
 GROUP BY activity
@@ -13,6 +14,24 @@ HAVING COUNT(id) > (SELECT COUNT(id)
                     GROUP BY activity
                     ORDER BY COUNT(id) DESC
                     LIMIT 1);
+                    
+                    
+# Method 2    
+SELECT name AS activity
+FROM Activities
+WHERE name <> (SELECT activity
+               FROM Friends
+               GROUP BY activity
+               ORDER BY COUNT(id) DESC
+               LIMIT 1) AND
+      name <> (SELECT activity
+               FROM Friends
+               GROUP BY activity
+               ORDER BY COUNT(id)
+               LIMIT 1)
+               
+               
+               
                     
                     
                     
