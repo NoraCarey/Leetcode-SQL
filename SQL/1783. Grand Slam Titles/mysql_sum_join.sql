fb5f796a9afced2eb9a9d3defa7f08e706d33b83
NoraCarey <46361714+NoraCarey@users.noreply.github.com>
2021-04-25 12:06:58 -0400

    Create mysql_sum_join.sql

---
+++ SQL/1783. Grand Slam Titles/mysql_sum_join.sql
@@ -0,0 +1,9 @@
+
+
+SELECT player_id, player_name, 
+       SUM(player_id = Wimbledon) + SUM(player_id = Fr_open) + SUM(player_id = US_open) + SUM(player_id = Au_open) AS grand_slams_count
+FROM Players p
+JOIN Championships c ON player_id = Wimbledon OR player_id = Fr_open OR player_id = US_open OR player_id = Au_open
+GROUP BY player_id, player_name;
+
+
