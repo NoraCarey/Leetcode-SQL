18b1fd43b43e513b8c3e0126fd650b01ed5e40a4
NoraCarey <46361714+NoraCarey@users.noreply.github.com>
2021-04-26 09:56:31 -0400

    Update mysql_coalesce_limit_offset.sql

--- SQL/0176. Second Highest Salary/mysql_coalesce_limit_offset.sql
+++ SQL/0176. Second Highest Salary/mysql_coalesce_limit_offset.sql
@@ -1,10 +1,11 @@
 
+# The number of Limit start from 0
 
 SELECT COALESCE((SELECT DISTINCT Salary
                  FROM Employee
                  ORDER BY Salary DESC
-                 LIMIT 1, 1
-                 # LIMIT 1 OFFSET 1), NULL) AS SecondHighestSalary;
+                 # LIMIT 1 OFFSET 
+                 LIMIT 1, 1), NULL) AS SecondHighestSalary;
                  
                  
                  
