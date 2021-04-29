55ad40c0e46a30c98bf52f6431fa26d467d86506
NoraCarey <46361714+NoraCarey@users.noreply.github.com>
2021-04-29 11:22:14 -0400

    Update mysql_case_expression.sql

--- SQL/0262. Trips and Users/mysql_case_expression.sql
+++ SQL/0262. Trips and Users/mysql_case_expression.sql
@@ -1,13 +1,10 @@
 
 
-SELECT t.Request_at AS Day, 
-       ROUND((SUM(CASE WHEN t.Status != 'completed' THEN 1 ELSE 0 END) / COUNT(t.Id)), 2) AS 'Cancellation Rate'
-FROM Trips t
-LEFT JOIN Users u1 ON t.Client_Id = u1.Users_Id
-LEFT JOIN Users u2 ON t.Driver_Id = u2.Users_id
-WHERE u1.Banned = 'No' # AND u2.Banned = 'No' 
-      AND t.Request_at >= '2013-10-01' AND t.Request_at <= '2013-10-03'
-GROUP BY t.Request_at
-ORDER BY t.Request_at;
-
+SELECT Request_at AS Day, 
+       ROUND(SUM(CASE WHEN Status != 'completed' THEN 1 ELSE 0 END) / COUNT(Id), 2) AS 'Cancellation Rate'
+FROM Trips 
+WHERE Request_at BETWEEN "2013-10-01" AND "2013-10-03" AND
+      Client_Id IN (SELECT Users_Id FROM Users WHERE Banned = 'No') AND
+      Driver_Id IN (SELECT Users_Id FROM Users WHERE Banned = 'No')
+GROUP BY Request_at;
 
