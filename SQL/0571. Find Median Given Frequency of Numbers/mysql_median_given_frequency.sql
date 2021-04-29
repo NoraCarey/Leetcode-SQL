6212a056c7ce38c3ff87cfb7182d9772d11f8a81
NoraCarey <46361714+NoraCarey@users.noreply.github.com>
2021-04-29 15:01:31 -0400

    Update mysql_median_given_frequency.sql

--- SQL/0571. Find Median Given Frequency of Numbers/mysql_median_given_frequency.sql
+++ SQL/0571. Find Median Given Frequency of Numbers/mysql_median_given_frequency.sql
@@ -3,9 +3,11 @@
 -- So it is the median in this array!
 
 -- What if an array has even amount of distinct values, the median is the average of the middle two elements next to each other after sorting this array. 
--- For example, [1, 5, 2, 7], the median number for this array should be (2 + 5) / 7. For 2, only 1 is smaller than it, and 5, 7 is bigger than it. 
--- For 5, 1 and 2 is smaller than it, only 7 is bigger than it. Either of 2 or 5, the difference(absolute value) of its bigger and smaller number than itself 
--- in this array is 1, which is the exactly frequency of a element in the distinct array.
+-- For example, [1, 5, 2, 7], the median number for this array should be (2 + 5) / 2. 
+-- For 2, only 1 is smaller than it, and 5, 7 is bigger than it. 
+-- For 5, 1 and 2 is smaller than it, only 7 is bigger than it. 
+-- Either of 2 or 5, the difference(absolute value) of its bigger and smaller number than itself in this array is 1, 
+-- which is the exactly frequency of a element in the distinct array.
 
 -- So in general, the median's frequency should be equal or grater than the absolute difference of its bigger elements and small ones in an array no matter 
 -- whether it has odd or even amount of numbers and whether they are distinct. This rule is the key, and it is represented as the following code.
