718fce5c39a8fb5eabd5e34bd2317322f072ffaa
NoraCarey <46361714+NoraCarey@users.noreply.github.com>
2021-04-30 10:35:15 -0400

    Create mysql_not_in_subquery.sql

---
+++ SQL/0584. Find Customer Referee/mysql_not_in_subquery.sql
@@ -0,0 +1,11 @@
+
+
+
+SELECT name
+FROM customer
+WHERE id NOT IN (SELECT id
+                 FROM customer
+                 WHERE referee_id = 2);
+                 
+                 
+                 
