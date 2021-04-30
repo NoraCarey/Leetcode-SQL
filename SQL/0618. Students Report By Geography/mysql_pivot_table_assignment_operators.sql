955c0f98351d0dad5f4dbd7a32fba00c882fbda2
NoraCarey <46361714+NoraCarey@users.noreply.github.com>
2021-04-30 16:19:43 -0400

    Update mysql_pivot_table_assignment_operators.sql

--- SQL/0618. Students Report By Geography/mysql_pivot_table_assignment_operators.sql
+++ SQL/0618. Students Report By Geography/mysql_pivot_table_assignment_operators.sql
@@ -13,13 +13,13 @@ FROM (
                   WHEN continent = 'America' THEN @r1 := @r1 + 1   -- assign correct row_num for each column
                   WHEN continent = 'Asia' THEN @r2 := @r2 + 1
                   WHEN continent = 'Europe' THEN @r3 := @r3 + 1
-             END AS 'row_id',
+             END AS 'row_num',
              CASE WHEN continent = 'America' THEN name END AS America,     -- put name to the correct alias
              CASE WHEN continent = 'Asia' THEN name END AS Asia,
              CASE WHEN continent = 'Europe' THEN name END AS Europe
       FROM student, 
            (SELECT @r1 := 0, @r2 := 0, @r3 := 0) a    -- use assignment operators := to set the user variables
       ORDER BY name) b
-GROUP BY row_id;
+GROUP BY row_num;
 
 
