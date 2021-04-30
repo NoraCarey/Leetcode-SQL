3841e138eb5b39079e9daa37c97df3042e120fa3
NoraCarey <46361714+NoraCarey@users.noreply.github.com>
2021-04-30 15:27:29 -0400

    Update mysql_self_join_pow_sqrt.sql

--- SQL/0612. Shortest Distance in a Plane/mysql_self_join_pow_sqrt.sql
+++ SQL/0612. Shortest Distance in a Plane/mysql_self_join_pow_sqrt.sql
@@ -1,7 +1,6 @@
 
 
-SELECT ROUND(MIN(SQRT(POW(p1.x - p2.x, 2) + POW(p1.y - p2.y, 2))), 2) AS shortest
-FROM point_2d p1, point_2d p2
-WHERE p1.x != p2.x OR p1.y != p2.y
-
+SELECT ROUND(MIN(SQRT(POW(ABS(t1.x - t2.x), 2) + POW(ABS(t1.y - t2.y), 2))), 2)AS shortest
+FROM point_2d t1, point_2d t2
+WHERE (t1.x, t1.y) != (t2.x, t2.y);
 
