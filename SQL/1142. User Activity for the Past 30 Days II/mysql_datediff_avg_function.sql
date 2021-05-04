53875aecc6f0d214c4c4dd8e731c8c0f838ffdaa
NoraCarey <46361714+NoraCarey@users.noreply.github.com>
2021-05-04 11:57:57 -0400

    Update and rename mysql_subquery_ifnull.sql to mysql_datediff_avg_function.sql

--- SQL/1142. User Activity for the Past 30 Days II/mysql_datediff_avg_function.sql
+++ SQL/1142. User Activity for the Past 30 Days II/mysql_datediff_avg_function.sql
@@ -1,11 +1,10 @@
 
 
 
-SELECT IFNULL(ROUND(SUM(session_count) / COUNT(user_id), 2), 0) AS average_sessions_per_user
+SELECT ROUND(IFNULL(AVG(session_count), 0), 2) AS average_sessions_per_user
 FROM (SELECT user_id, COUNT(DISTINCT session_id) AS session_count
       FROM Activity
       WHERE DATEDIFF('2019-07-27', activity_date) < 30
       GROUP BY user_id) a
-      
       
       
