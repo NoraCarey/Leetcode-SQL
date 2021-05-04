40075810c18118ad42d46b3fa98046dceabc100b
NoraCarey <46361714+NoraCarey@users.noreply.github.com>
2021-05-04 14:56:56 -0400

    Update mysql_rank_window_function_left_join.sql

--- SQL/1164. Product Price at a Given Date/mysql_rank_window_function_left_join.sql
+++ SQL/1164. Product Price at a Given Date/mysql_rank_window_function_left_join.sql
@@ -1,16 +1,17 @@
 
 
-SELECT a.product_id, 
-       IFNULL(new_price, 10) AS price
-FROM (SELECT DISTINCT(product_id), 10 AS price
-      FROM Products) a
-LEFT JOIN (SELECT product_id, new_price 
-           FROM (SELECT product_id, new_price, 
-                        RANK() OVER(PARTITION BY product_id
-                                    ORDER BY change_date DESC) AS ranking
-                 FROM Products
-                 WHERE change_date <= '2019-08-16') b
-          WHERE ranking = 1) c
-ON a.product_id = c.product_id;
+SELECT b.product_id, 
+       CASE
+           WHEN a.price IS NOT NULL THEN price
+           ELSE 10
+       END AS price
+FROM (SELECT product_id, new_price AS price, 
+             ROW_NUMBER() OVER(PARTITION BY product_id
+                               ORDER BY change_date DESC) AS ranking
+      FROM Products
+      WHERE change_date <= '2019-08-16') a
+RIGHT JOIN (SELECT DISTINCT product_id
+            FROM Products) b
+ON b.product_id = a.product_id AND ranking = 1;
 
 
