972512b613651f29b492adff3f8089d8c513365c
NoraCarey <46361714+NoraCarey@users.noreply.github.com>
2021-05-04 12:04:33 -0400

    Update mysql_distinct_group_by_having.sql

--- SQL/1149. Article Views II/mysql_distinct_group_by_having.sql
+++ SQL/1149. Article Views II/mysql_distinct_group_by_having.sql
@@ -2,10 +2,9 @@
 
 
 SELECT DISTINCT viewer_id AS id
-FROM (SELECT DISTINCT *
-      FROM Views) a
+FROM Views
 GROUP BY viewer_id, view_date
-HAVING COUNT(article_id) > 1
+HAVING COUNT(DISTINCT article_id) > 1
 ORDER BY id;
 
 
