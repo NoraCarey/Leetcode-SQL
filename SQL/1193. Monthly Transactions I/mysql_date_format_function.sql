a857b0249c4e9672742376789b8a8fc01eeb76fd
NoraCarey <46361714+NoraCarey@users.noreply.github.com>
2021-05-05 22:32:16 -0400

    Update mysql_date_format_function.sql

--- SQL/1193. Monthly Transactions I/mysql_date_format_function.sql
+++ SQL/1193. Monthly Transactions I/mysql_date_format_function.sql
@@ -1,9 +1,9 @@
 
 
-SELECT DATE_FORMAT(trans_date, '%Y-%m') AS month, country, COUNT(id) AS trans_count,
-       SUM(CASE WHEN state = 'approved' THEN 1 ELSE 0 END) AS approved_count, 
-       IFNULL(SUM(amount), 0) AS trans_total_amount, 
-       IFNULL(SUM(CASE WHEN state = 'approved' THEN amount END), 0) AS approved_total_amount
+SELECT DATE_FORMAT(trans_date, '%Y-%m') AS month, country, COUNT(id) AS trans_count, 
+       SUM(CASE WHEN state = 'approved' THEN 1 ELSE 0 END) AS approved_count,
+       SUM(amount) AS trans_total_amount, 
+       SUM(CASE WHEN state = 'approved' THEN amount ELSE 0 END) AS approved_total_amount
 FROM Transactions
 GROUP BY month, country;
 
