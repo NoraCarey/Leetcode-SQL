e85acae1332338c3e954445695bef0f56f6b4af0
NoraCarey <46361714+NoraCarey@users.noreply.github.com>
2021-05-07 17:51:48 -0400

    Update mysql_union_right_join.sql

--- SQL/1435. Create a Session Bar Chart/mysql_union_right_join.sql
+++ SQL/1435. Create a Session Bar Chart/mysql_union_right_join.sql
@@ -1,23 +1,22 @@
 
 
-SELECT b.bin, IFNULL(COUNT(a.session_id), 0) AS Total
-FROM (SELECT session_id, 
-             CASE 
+SELECT b.bin, IFNULL(total, 0) AS total
+FROM (SELECT CASE
                  WHEN duration / 60 >= 0 AND duration / 60 < 5 THEN '[0-5>'
-                 WHEN duration / 60 >= 5 AND duration / 60 < 10 THEN '[5-10> '
+                 WHEN duration / 60 >= 5 AND duration / 60 < 10 THEN '[5-10>'
                  WHEN duration / 60 >= 10 AND duration / 60 < 15 THEN '[10-15>'
-                 WHEN duration / 60 >= 15 THEN '15 or more'
-             END bin
-      FROM Sessions) a
+                 ELSE '15 or more'
+              END bin, COUNT(session_id) AS total
+      FROM Sessions
+      GROUP BY bin) a
 RIGHT JOIN (SELECT '[0-5>' AS bin
-            UNION 
+            UNION
             SELECT '[5-10>' AS bin
             UNION
             SELECT '[10-15>' AS bin
             UNION
             SELECT '15 or more' AS bin) b
-ON a.bin = b.bin
-GROUP BY b.bin;   
+ON a.bin = b.bin;
 
 
 
