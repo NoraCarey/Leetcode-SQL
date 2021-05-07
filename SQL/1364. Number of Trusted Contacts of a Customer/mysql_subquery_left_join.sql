323094ff92b1fccda29ac0cdd60abbe26b89d1c4
NoraCarey <46361714+NoraCarey@users.noreply.github.com>
2021-05-07 15:27:26 -0400

    Update mysql_subquery_left_join.sql

--- SQL/1364. Number of Trusted Contacts of a Customer/mysql_subquery_left_join.sql
+++ SQL/1364. Number of Trusted Contacts of a Customer/mysql_subquery_left_join.sql
@@ -2,14 +2,12 @@
 
 
 SELECT invoice_id, customer_name, price, 
-       IFNULL(COUNT(contact_name), 0) AS contacts_cnt, 
-       IFNULL(SUM(CASE WHEN contact_name IN (SELECT customer_name
-                                             FROM Customers) 
-                       THEN 1 ELSE 0 END), 0) AS trusted_contacts_cnt
+       COUNT(contact_name) AS contacts_cnt, 
+       SUM(CASE WHEN contact_email IN (SELECT email FROM Customers) THEN 1 ELSE 0 END) AS trusted_contacts_cnt
 FROM Invoices i
-JOIN Customers c ON i.user_id = c.customer_id
-LEFT JOIN Contacts co ON c.customer_id = co.user_id
-GROUP BY invoice_id, customer_name, price
+LEFT JOIN Customers cu ON i.user_id = cu.customer_id
+LEFT JOIN Contacts co ON cu.customer_id = co.user_id
+GROUP BY invoice_id, customer_name
 ORDER BY invoice_id;
 
 
