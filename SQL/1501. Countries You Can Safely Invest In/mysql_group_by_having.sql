b87df38cad3b94eace99235fdf621fa331dd733d
NoraCarey <46361714+NoraCarey@users.noreply.github.com>
2021-05-08 12:23:22 -0400

    Update mysql_group_by_having.sql

--- SQL/1501. Countries You Can Safely Invest In/mysql_group_by_having.sql
+++ SQL/1501. Countries You Can Safely Invest In/mysql_group_by_having.sql
@@ -1,10 +1,10 @@
 
 
-SELECT co.name AS country
+SELECT DISTINCT c.name AS country
 FROM Person p
-JOIN Country co ON LEFT(p.phone_number, 3) = co.country_code
-JOIN Calls ca ON p.id = ca.caller_id OR p.id = ca.callee_id
-GROUP BY co.name
-HAVING AVG(duration) > (SELECT SUM(duration) / COUNT(*) FROM Calls);
+JOIN Calls l ON caller_id = id OR id = callee_id
+JOIN Country c ON LEFT(phone_number, 3) = country_code
+GROUP BY country_code
+HAVING AVG(duration) > (SELECT AVG(duration) FROM Calls)
 
 
