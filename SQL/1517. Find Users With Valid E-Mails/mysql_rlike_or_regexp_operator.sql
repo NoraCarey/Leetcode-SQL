d8911871baa5f7ce8ddf121af888a5d34ed3a285
NoraCarey <46361714+NoraCarey@users.noreply.github.com>
2021-05-08 20:20:37 -0400

    Update mysql_rlike_or_regexp_operator.sql

--- SQL/1517. Find Users With Valid E-Mails/mysql_rlike_or_regexp_operator.sql
+++ SQL/1517. Find Users With Valid E-Mails/mysql_rlike_or_regexp_operator.sql
@@ -2,8 +2,8 @@
 
 SELECT *
 FROM Users 
-# WHERE mail RLIKE '^[A-Za-z][A-Za-z0-9\_\.\-]*@leetcode.com$';
-WHERE mail REGEXP '^[A-Za-z][A-Za-z0-9\_\.\-]*@leetcode.com$';
+# WHERE mail RLIKE '^[A-Za-z][A-Za-z0-9\\_\\.\\-]*@leetcode.com$';
+WHERE mail REGEXP '^[A-Za-z][A-Za-z0-9\\_\\.\\-]*@leetcode.com$';
 
 
 
