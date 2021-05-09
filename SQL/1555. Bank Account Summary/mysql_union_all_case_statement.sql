b19c914cd5e1a6c9a89621d076a90cb39b3bddff
NoraCarey <46361714+NoraCarey@users.noreply.github.com>
2021-05-08 21:16:11 -0400

    Update mysql_union_all_case_statement.sql

--- SQL/1555. Bank Account Summary/mysql_union_all_case_statement.sql
+++ SQL/1555. Bank Account Summary/mysql_union_all_case_statement.sql
@@ -1,17 +1,11 @@
 
 
-SELECT DISTINCT u.user_id, user_name, (credit + IFNULL(amount, 0)) AS credit,
-       CASE 
-           WHEN  (credit + IFNULL(amount, 0)) >= 0 THEN 'No'
-           ELSE 'Yes'
-       END credit_limit_breached 
-FROM (SELECT user_id, SUM(amount) AS amount
-      FROM (SELECT paid_by AS user_id, (-amount) AS amount
-            FROM Transactions 
-            UNION ALL
-            SELECT paid_to AS user_id, amount
-            FROM Transactions) a
-      GROUP BY user_id) b
-RIGHT JOIN Users u ON u.user_id = b.user_id;
-
-
+SELECT u.user_id, user_name, (credit + IFNULL(SUM(amount), 0)) AS credit, 
+       CASE WHEN (credit + IFNULL(SUM(amount), 0)) < 0 THEN 'Yes' ELSE 'No' END AS credit_limit_breached 
+FROM (SELECT paid_by AS user_id, -amount AS amount
+      FROM Transactions
+      UNION ALL
+      SELECT paid_to AS user_id, amount AS amount
+      FROM Transactions) a
+RIGHT JOIN Users u ON a.user_id = u.user_id
+GROUP BY u.user_id;
