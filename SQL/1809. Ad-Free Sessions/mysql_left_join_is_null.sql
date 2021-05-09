2e1ba3ba0591ebb279d7e0541ddc5137495785de
NoraCarey <46361714+NoraCarey@users.noreply.github.com>
2021-05-09 18:50:35 -0400

    Create mysql_left_join_is_null.sql

---
+++ SQL/1809. Ad-Free Sessions/mysql_left_join_is_null.sql
@@ -0,0 +1,9 @@
+
+
+SELECT session_id
+FROM Playback p
+LEFT JOIN Ads a ON p.customer_id = a.customer_id AND
+                   (timestamp BETWEEN start_time AND end_time)
+WHERE a.customer_id IS NULL
+
+
