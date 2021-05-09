b11681c74f33057ea2c49482607be70a6edcbd7e
NoraCarey <46361714+NoraCarey@users.noreply.github.com>
2021-05-09 13:00:29 -0400

    Update mysql_self_join_between_and.sql

--- SQL/1747. Leetflex Banned Accounts/mysql_self_join_between_and.sql
+++ SQL/1747. Leetflex Banned Accounts/mysql_self_join_between_and.sql
@@ -2,9 +2,9 @@
 
 SELECT DISTINCT t1.account_id
 FROM LogInfo t1, LogInfo t2
-WHERE t1.account_id = t2.account_id AND t1.ip_address < t2.ip_address AND
-      (t1.login BETWEEN t2.login AND t2.logout OR 
-       t2.login BETWEEN t1.login AND t1.logout);
+WHERE t1.account_id = t2.account_id AND t1.ip_address != t2.ip_address AND
+      t2.login BETWEEN t1.login AND t1.logout;
+
        
        
        
