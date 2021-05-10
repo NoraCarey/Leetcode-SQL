d573a518330feebd0809040b863ec795c62a0b2f
NoraCarey <46361714+NoraCarey@users.noreply.github.com>
2021-05-10 12:22:11 -0400

    Update mysql_dense_rank_window_function.sql

--- SQL/0178. Rank Scores/mysql_dense_rank_window_function.sql
+++ SQL/0178. Rank Scores/mysql_dense_rank_window_function.sql
@@ -1,4 +1,5 @@
-
+# DENSE_RANK() makes sure after a tie, the next ranking number should be the next consecutive integer value. 
+# In other words, there should be no "holes" or "gaps" between ranks.
 
 SELECT Score, DENSE_RANK() OVER (ORDER BY score DESC) AS 'Rank'
 FROM Scores;
