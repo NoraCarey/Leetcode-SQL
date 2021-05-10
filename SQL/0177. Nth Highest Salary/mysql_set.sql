f7205c67e0c261f7baa00757da48bcce80dfa258
NoraCarey <46361714+NoraCarey@users.noreply.github.com>
2021-05-10 12:16:08 -0400

    Update mysql_set.sql

--- SQL/0177. Nth Highest Salary/mysql_set.sql
+++ SQL/0177. Nth Highest Salary/mysql_set.sql
@@ -2,6 +2,7 @@
 # Becasue N is a Local Variable Assignment, it is not a User-defined Variable Assignment, there is no need using @ here
 # It is just SET N := N - 1, ('=' AND ':=' is the same)
 
+# Calculations could not be used inside the RETRUN block, we need to do the calculations outside the block. 
 
 CREATE FUNCTION getNthHighestSalary(N INT) 
 RETURNS INT
